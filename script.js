439c201c523ac6b18faf3468c860a9b2ad91d164
Anastasiiasyvak <asyvak@kse.org.ua>
2023-09-26 21:04:45 +0300

    ended_project

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 
 function concatenateString(text, maxLength) {
-    if (typeof text !== 'string' || typeof maxLength !== 'number') {
-        return "Invalid input"; 
-    }
     if (text.length <= maxLength) {
         return text;  
     } 
@@ -10,25 +7,16 @@ function concatenateString(text, maxLength) {
         return text.slice(0, maxLength).toUpperCase() + '...';
     }
 }
-
 console.log(concatenateString('Longer than expected', 5));  
-
 
 function createTableCells(){
 
     const rowAmountInput = document.getElementById("rowAmount");
     const cellsAmountInput = document.getElementById("cellsAmount");
-
-    const rowAmount = parseInt(rowAmountInput.value);
+    const rowAmount = parseInt(rowAmountInput.value); // parseint конвертує в цілі числа 
     const cellsAmount = parseInt(cellsAmountInput.value);
-
-
-    if (typeof rowAmount !== "number" || typeof cellsAmount !== "number"){
-        return "Invalid input"
-    }
-
-     const tbl = document.createElement("table");
-     const tblBody = document.createElement("tbody");
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
    
      for (let i = 0; i < rowAmount; i++) {
        const row= document.createElement("tr");
@@ -52,6 +40,7 @@ function removeTable() {
     $("table").remove();
 }
 
+
 class Book {
     constructor(title, authors, numberOfPages, isRead, isFavorite) {
         this.title = title;
@@ -70,16 +59,126 @@ class Book {
     }
 }
 
-let bookInstance = new Book('LalaLend', 'Brown, Tim del Ray', 200, false, false);
+class Bookshelf {
+    constructor(books = []) {
+        this.books = books;
+    }
 
-console.log('Title:', bookInstance.title);
-console.log('Authors:', bookInstance.authors);
-console.log('Number of Pages:', bookInstance.numberOfPages);
-console.log('Is Read:', bookInstance.isRead);
-console.log('Is Favorite:', bookInstance.isFavorite);
+    addBook(book) {
+        this.books.push(book);
+    }
 
-bookInstance.markAsRead();
-console.log('Is Read:', bookInstance.isRead); 
+    removeBook(book) {
+        const index = this.books.indexOf(book);
+        if (index !== -1) { // и перевіряємо чи книга в масиві бо якщо ні то позиція книги -1 
+            this.books.splice(index, 1); // splice идаляє
+        }
+    }
 
-bookInstance.toggleFavorite();
-console.log('Is Favorite:', bookInstance.isFavorite); 
+    getUnreadBooks() {
+        return this.books.filter(book => !book.isRead);
+    }
+
+    getFavBooks() {
+        return this.books.filter(book => book.isFavorite);
+    }
+
+    getTotalBooksCount() {
+        return this.books.length;
+    }
+}
+
+
+function displayBooks(books) {
+    const bookList = document.getElementById("bookList");
+    bookList.innerHTML = ""; // коли оновимо сторіночку, список зникне 
+
+    books.forEach(book => {
+        const bookItem = document.createElement("div");
+        bookItem.classList.add("book-item");
+        bookItem.innerHTML = `
+            <p>Title: ${book.title}</p>
+            <p>Authors: ${book.authors}</p>
+            <p>Number of Pages: ${book.numberOfPages}</p>
+            <p>Favorite: ${book.isFavorite ? 'Yes' : 'No'}</p>
+            <p>Is Read: ${book.isRead ? 'Yes' : 'No'}</p>
+        <button class="button3" onclick="toggleFavorite('${book.title}')">Toggle Favorite</button>
+        <button class="button3" onclick="markAsRead('${book.title}')">Mark as Read</button>
+        <button class="button4" onclick="removeBook('${book.title}')">Remove</button>
+        `;
+
+        bookList.appendChild(bookItem);
+    });
+}
+
+
+function addNewBook() {
+    const titleInput = document.getElementById("title");
+    const authorsInput = document.getElementById("authors");
+    const numberOfPagesInput = document.getElementById("numberOfPages");
+
+    const title = titleInput.value;
+    const authors = authorsInput.value;
+    const numberOfPages = parseInt(numberOfPagesInput.value);
+    const isRead = false;
+    const isFavorite = false;
+
+    const newBook = new Book(title, authors, numberOfPages, isRead, isFavorite);
+    shelf.addBook(newBook);
+    updateTotalBooksCount();
+    displayBooks(shelf.books);
+
+    titleInput.value = "";
+    authorsInput.value = "";
+    numberOfPagesInput.value = "";
+}
+
+
+function updateTotalBooksCount() {
+    const totalBooksCount = document.getElementById("totalBooksCount");
+    totalBooksCount.textContent = `Total amount of books: ${shelf.getTotalBooksCount()}`;
+}
+
+
+function alertFavoriteBooks() {
+    const favoriteBooks = shelf.getFavBooks();
+    alert(`Number of favorite books: ${favoriteBooks.length}`);
+}
+
+
+function alertUnreadBooks() {
+    const unreadBooks = shelf.getUnreadBooks();
+    alert(`Number of unread books: ${unreadBooks.length}`);
+}
+
+
+function removeBook(title) {
+    const bookToRemove = shelf.books.find(book => book.title === title);
+    if (bookToRemove) {
+        shelf.removeBook(bookToRemove);
+        updateTotalBooksCount();
+        displayBooks(shelf.books); 
+    }
+}
+
+
+function toggleFavorite(title) {
+    const bookToToggle = shelf.books.find(book => book.title === title);
+    if (bookToToggle) {
+        bookToToggle.toggleFavorite();
+        displayBooks(shelf.books); 
+    }
+}
+
+
+function markAsRead(title) {
+    const bookToMarkAsRead = shelf.books.find(book => book.title === title);
+    if (bookToMarkAsRead) {
+        bookToMarkAsRead.markAsRead();
+        displayBooks(shelf.books); 
+    }
+}
+
+
+const shelf = new Bookshelf([]);
+updateTotalBooksCount();
